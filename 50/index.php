c392c60debb70756fc2a1686b3930889d6a8567d
Taehyun Sung <alexseong1004@gmail.com>
2024-01-05 09:58:07 +0800

    tutorial 50 finished

--- 50/index.php
+++ 50/index.php
@@ -1,5 +1,13 @@
 <?php
+$sentence = 'this is a sentence and I want you to replace the text
+that i am about to type. Please be mindful';
+$word = 'mindful';
+$replace_text = 'careful';
 
+$position_index = strpos($sentence, $word);
+echo $position_index;
 
+$new_sentence = substr_replace($sentence, $replace_text, $position_index, strlen($replace_text));
 
+//echo $new_sentence;
 ?>
